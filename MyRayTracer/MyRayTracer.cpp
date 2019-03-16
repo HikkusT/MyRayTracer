b496a3da3985dcd60a95ae3e967cf77a1d723c50
HikkusT <luizhenrique98@gmail.com>
2019-03-16 15:46:01 -0300

    Remove deafult comments

--- MyRayTracer/MyRayTracer.cpp
+++ MyRayTracer/MyRayTracer.cpp
@@ -1,6 +1,3 @@
-// MyRayTracer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
 #include <fstream>
@@ -12,6 +9,8 @@ void render()
 	const int width = 1024;
 	const int height = 768;
 
+
+	//Creating Image
 	ofstream ofs;
 	ofs.open("./output.ppm");
 	ofs << "P6\n" << width << ' ' << height << "\n255\n";
@@ -26,14 +25,3 @@ int main()
 	render();
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
