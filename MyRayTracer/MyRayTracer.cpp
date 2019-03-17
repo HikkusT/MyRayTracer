5a86775913fa302dbb913c65808ac978f2b8da63
HikkusT <luizhenrique98@gmail.com>
2019-03-17 03:37:38 -0300

    Create first image

--- MyRayTracer/MyRayTracer.cpp
+++ MyRayTracer/MyRayTracer.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include "geometry.h"
 
 using namespace std;
 
@@ -8,15 +10,22 @@ void render()
 {
 	const int width = 1024;
 	const int height = 768;
+	vector<Vec3f> framebuffer(width * height);
 
+	for (int i = 0; i < height; i++)
+		for (int j = 0; j < width; j++)
+			framebuffer[i * width + j] = Vec3f(i / float(height), 0, j / float(width));
 
 	//Creating Image
 	ofstream ofs;
 	ofs.open("./output.ppm");
-	ofs << "P6\n" << width << ' ' << height << "\n255\n";
-	for (int size = 0; size < width * height; size++)
-		for (int i = 0; i < 3; i++)
-			ofs << (char)(0);
+	ofs << "P3\n" << width << " " << height << "\n255\n";
+	for (int i = 0; i < width * height; i++)
+	{
+		for (int j = 0; j < 3; j++)
+			ofs << (int)(255 * framebuffer[i][j]) << " ";
+		ofs << "\n";
+	}
 	ofs.close();
 }
 
